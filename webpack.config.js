373527b7cc3a0da57b851506263e1f49c36edcaf
Чечеринда Вадим <checherinda.vadym@gmail.com>
2017-05-02 23:27:27 +0300

    Now can add pictures in dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Для подключения *.html templates
+const ExtractTextPlugin = require("extract-text-webpack-plugin"); // Для подключения *.css через <link>, а не через <style> 
+const webpack = require('webpack');
+const path = require('path');
+
+const isProd = process.argv.indexOf('-p') !== -1; // true or false
+const cssDev = ['style-loader','css-loader','sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+	fallback: 'style-loader',
+	use: ['css-loader','sass-loader']
+})
+
+const cssConfig = isProd ? cssProd : cssDev;
 
 module.exports = {
 	entry: {
@@ -15,21 +26,38 @@ module.exports = {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'sass-loader']
-				})
+				// use: ExtractTextPlugin.extract({
+				// 	fallback: 'style-loader',
+				// 	use: ['css-loader', 'sass-loader']
+				// })
+				use: cssConfig
 			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: 'babel-loader'
-			}
+			},
+			{
+				test: /\.pug$/,
+				use: 'pug-html-loader'
+			},
+			{
+		        test: /\.(jpe?g|png|gif|svg)$/i,
+		        use: [
+   					'file-loader?name=img/[name].[ext]',
+   					// 'file-loader?name=[name].[ext]&outputPath=img/&publicPath=img/',
+			        {
+				      loader: 'image-webpack-loader',
+				      options: {}
+				    }
+				]    
+	      	}﻿
 		]
 	},
 	devServer: {
 		contentBase: __dirname + "/dist",
   		compress: true,
+  		hot: true,
   		stats: 'errors-only' // Show only errors
 	},
 	plugins: [
@@ -44,6 +72,12 @@ module.exports = {
 			filename: 'contact.html',
 			template: './src/contact.html' 
 		}),
-		new ExtractTextPlugin('style.css')
+		new ExtractTextPlugin({
+			filename: 'style.css',
+			disable: !isProd
+		}),
+		
+		new webpack.HotModuleReplacementPlugin(),
+    	new webpack.NamedModulesPlugin()
 	]
 }
